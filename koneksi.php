43a3ec7bd7a6ce24add8ec2657d3d03caf9bb5a4
Rizqi Arif <rizkyarif0106@gmail.com>
2025-10-06 11:21:56 +0700

    first commit

--- koneksi.php
+++ koneksi.php
@@ -1,12 +1,12 @@
 <?php
-$host = "localhost";
-$user = "xirpl1-31";
-$pass = "0081441342";
-$db   = "sewa_outdoor_db";
+$host = "localhost"; 
+$user = "xirpl1-31"; 
+$pass = "0801414342"; 
+$db   = "db_xirpl1-31_2";
 
 $conn = mysqli_connect($host, $user, $pass, $db);
 
 if (!$conn) {
-  die("Koneksi gagal: " . mysqli_connect_error());
+    die("Koneksi gagal: " . mysqli_connect_error());
 }
 ?>
